4d315b4c24d0c4490a94ea64ceb8919e10e3c6ba
Kevin-XPS <kevin.chen@innopeaktech.com>
2020-04-03 23:51:43 -0700

    feat: upload prebuilt ceres libraries

---
+++ build.sh
@@ -0,0 +1,10 @@
+if [ ! -d "build" ]; then
+    mkdir build
+else
+    rm -rf build/*
+fi
+
+cd build
+
+cmake ..
+make -j4
---
+++ test/test_ceres.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <chrono>
+
+#include "Eigen/Core"
+#include "ceres/ceres.h"
+
+int main (int argc, char** argv) 
+{
+   ceres::Solver::Options options;
+
+    return 0;
+}
